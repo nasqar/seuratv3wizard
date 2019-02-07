2ddc22ca5afac28a2ae17bfa62ee89e809ffce67
aymanm <ayman.yousif@gmail.com>
2019-02-07 10:19:48 +0400

    Fix export function

--- R/SeuratWizard.R
+++ R/SeuratWizard.R
@@ -1,11 +1,11 @@
 #' @export
-SeuratWizard <- function(ip=NULL,port=NULL){
+SeuratV3Wizard <- function(ip=NULL,port=NULL){
   if(is.null(ip))
     ip = '0.0.0.0'
 
   if(is.null(port))
     port = 1234
 
-  appDir <- system.file('shiny', package = "SeuratWizard")
+  appDir <- system.file('shiny', package = "SeuratV3Wizard")
   shiny::runApp(appDir,host = getOption('shiny.host', ip),port = port,launch.browser = FALSE)
 }
